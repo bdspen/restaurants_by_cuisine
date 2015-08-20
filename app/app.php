117610c9792d3705b1f3d06018d4ad41fd7fadb4
Ben Spenard & Kyle Pratuch <student@epicodus.com>
2015-08-20 10:19:27 -0700

    add page routes in app.php

--- app/app.php
+++ app/app.php
@@ -26,6 +26,34 @@
 
     $app->get("/cuisines/{id}", function($id) use ($app) {
         return $app['twig']->render('cuisine.html.twig', array('cuisines' => $cuisine, 'restaurants' => $cuisine->getRestaurants()));
-    })
+    });
+
+    $app->post("/restaurants", function() use ($app) {
+        $name = $_POST['name'];
+        $cuisine_id = $_POST['cuisine'];
+        $phone = $_POST['phone'];
+        $price = $_POST['price'];
+        $restaurant = new Restaurant($name, $phone, $price, $cuisine_id);
+        $restaurant->save();
+        $cuisine = Cuisine::find($cuisine_id);
+        return $app['twig']->render('cuisine.html.twig', array('cuisine' =>$cuisine, 'restaurants' => Restaurant::getAll()));
+    });
+
+    $app->post("/restaurants", function() use ($app) {
+        Restaurant::deleteAll();
+        return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll()));
+    });
+
+    $app->post("/delete_cuisines", function() use ($app) {
+        Restaurants::deleteAll();
+        Cuisine::deleteAll();
+        return $app['twig']->render('index.html.twig');
+    });
+
+    $app->post("/cuisines", function() use ($app) {
+        $cuisine = new Cuisine($_POST['type']);
+        $cuisine->save();
+        return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll()));
+    });
 
  ?>
