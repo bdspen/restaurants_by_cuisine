e56e2b15d85415d3937c69df9c3f5f49274da243
Ben Spenard & Kyle Pratuch <student@epicodus.com>
2015-08-20 12:26:01 -0700

    add twig templates

--- app/app.php
+++ app/app.php
@@ -6,6 +6,7 @@
     require_once __DIR__."/../src/Cuisine.php";
 
     $app = new Silex\Application();
+    $app['debug'] = true;
 
     $server = 'mysql:host=localhost;dbname=restaurants_by_cuisine';
     $username = 'root';
@@ -20,40 +21,46 @@
         return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll()));
     });
 
-    $app->get("/restaurants", function() use ($app) {
-        return $app['twig']->render('restaurants.html.twig', array('restaurants' => Restaurant::getAll()));
-    });
-
-    $app->get("/cuisines/{id}", function($id) use ($app) {
-        return $app['twig']->render('cuisine.html.twig', array('cuisines' => $cuisine, 'restaurants' => $cuisine->getRestaurants()));
-    });
-
-    $app->post("/restaurants", function() use ($app) {
-        $name = $_POST['name'];
-        $cuisine_id = $_POST['cuisine'];
-        $phone = $_POST['phone'];
-        $price = $_POST['price'];
-        $restaurant = new Restaurant($name, $phone, $price, $cuisine_id);
-        $restaurant->save();
-        $cuisine = Cuisine::find($cuisine_id);
-        return $app['twig']->render('cuisine.html.twig', array('cuisine' =>$cuisine, 'restaurants' => Restaurant::getAll()));
-    });
-
-    $app->post("/restaurants", function() use ($app) {
-        Restaurant::deleteAll();
-        return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll()));
-    });
-
-    $app->post("/delete_cuisines", function() use ($app) {
-        Restaurants::deleteAll();
-        Cuisine::deleteAll();
-        return $app['twig']->render('index.html.twig');
-    });
-
     $app->post("/cuisines", function() use ($app) {
         $cuisine = new Cuisine($_POST['type']);
         $cuisine->save();
         return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll()));
     });
+
+    $app->get("/cuisines/{id}", function($id) use ($app) {
+        $cuisine = Cuisine::find($id);
+        return $app['twig']->render('cuisine.html.twig', array('cuisine' => $cuisine, 'restaurants' => $cuisine->getRestaurants()));
+    });
+
+    $app->post("/restaurants", function() use ($app) {
+        $name = $_POST['name'];
+        $phone = $_POST['phone'];
+        $price = $_POST['price'];
+        $cuisine_id = $_POST['cuisine_id'];
+        $restaurant = new Restaurant($name, $phone, $price, $cuisine_id);
+        $restaurant->save();
+        $cuisine = Cuisine::find($cuisine_id);
+        return $app['twig']->render('cuisine.html.twig', array('cuisine' => $cuisine, 'restaurants' => $cuisine->getRestaurants()));
+    });
+
+    $app->get("/restaurants", function() use ($app) {
+        $cuisine = Cuisine::find($cuisine_id);
+        return $app['twig']->render('cuisine.html.twig', array('cuisine' => $cuisine, 'restaurants' => Restaurant::getAll()));
+    });
+
+    $app->post("/delete_restaurants", function() use ($app) {
+        Restaurant::deleteAll();
+        return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll()));
+    });
+
+    $app->post("/delete_cuisines", function() use ($app) {
+        Restaurant::deleteAll();
+        Cuisine::deleteAll();
+        return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll()));
+    });
+
+
+
+    return $app;
 
  ?>
